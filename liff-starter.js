d171f42373988997a479dbfdccf14ae9bfe863ad
aor1234 <58202304+aor1234@users.noreply.github.com>
2019-12-24 11:34:23 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -34,9 +34,9 @@ function initializeApp(data) {
             packageId: '2',
             stickerId: '144'
         }]).then(function () {
-            window.alert("Message sent");
+            
         }).catch(function (error) {
-            window.alert("Error sending message: " + error);
+            
         });
     });
 
