ad6fd8bbf68ec84225912b4268a2edeabd08b9ca
aor1234 <58202304+aor1234@users.noreply.github.com>
2019-12-24 16:06:01 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -13,7 +13,7 @@ function initializeApp(data) {
 
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {       
-        var msg = document.getElementById('bchcode').value;
+        var msg = 'Hello World';
         
         liff.sendMessages([{
             type: 'text',
