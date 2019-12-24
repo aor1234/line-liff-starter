63a4786aa97924d3dfd7991d28ecc28e19f51403
aor1234 <58202304+aor1234@users.noreply.github.com>
2019-12-24 12:25:25 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -14,9 +14,9 @@ function initializeApp(data) {
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
+            type: 'text',
             var a = document.getElementById('name').value,
-            var b = document.getElementById('fdate').value,
-            var c = document.getElementById('ldate').value
+          
         }, {
             type: 'sticker',
             packageId: '2',
